e4b335e99a00f5c9e2f6fbd3cc155b5107d1ebdf
Sean Madden <sdm350@gmail.com>
2015-11-09 21:31:19 -0800

    Refactor TrelloList class

--- src/model/TrelloList.kt
+++ src/model/TrelloList.kt
@@ -1,8 +1,25 @@
-public data class TrelloList(
-        val id: String,
-        var name: String,
-        var closed: Boolean?,
-        val idBoard: String,
-        var pos: Int,
-        var subscribed: Boolean
-)
+public class TrelloList(
+        var id: String = "",
+        var name: String = "",
+        var closed: Boolean? = false,
+        var idBoard: String = "",
+        var pos: Long = 65567L,
+        var subscribed: Boolean = false,
+        var cards: List<Card> = emptyList() ) {
+
+    init {
+        this.id = id
+        this.name = name
+        this.closed = closed
+        this.idBoard = idBoard
+        this.pos = pos
+        this.subscribed = subscribed
+        this.cards = cards
+    }
+
+    fun addCard(card: Card) {
+        cards += card
+    }
+
+
+}
