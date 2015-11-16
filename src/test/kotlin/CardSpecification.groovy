0b8c3d04554315f4553fd87afb990bfd3afdb6c4
Sean Madden <sdm350@gmail.com>
2015-11-15 22:55:18 -0800

    Add Mocks to Trello
    Mocking doesn't quite work as Spock doesn't want to mock what it thinks are final java classes.

--- src/test/kotlin/CardSpecification.groovy
+++ src/test/kotlin/CardSpecification.groovy
@@ -2,12 +2,24 @@ import spock.lang.Specification
 
 
 class CardSpecification extends Specification {
+    def trello = Mock(Trello)
+
     def "Card can be created"() {
         given:
         def card = new Card()
 
         expect:
         card.desc.isEmpty()
+        card.id.isEmpty()
+    }
+
+    def "Card can be created with an id and pulled from Trello"() {
+        given:
+        def card = new Card().id = "id-1"
+
+        expect:
+        card.id == "id-1"
+
     }
 
 }
