96600c47704dd43c63518639c5adbb4033213331
Sean Madden <sdm350@gmail.com>
2015-11-18 23:03:16 -0800

    Refeactor Checklist

--- src/model/Checklist.kt
+++ src/model/Checklist.kt
@@ -1,16 +1,9 @@
 class Checklist(
-    var id: String? = "",
+    var id: String = "",
     var name: String = "",
     var checkListItems: List<CheckListItem> = kotlin.emptyList(),
     var cards: List<CardMetadata> = kotlin.emptyList()
 ) {
-
-    init {
-        this.id = null
-        this.name = name
-        this.checkListItems = listOf()
-        this.cards = listOf()
-    }
 
     /**
      * Appends the given checklist item to the list
@@ -35,11 +28,4 @@ class CheckListItem(
         var nameData: String? = "",
         var pos: Long = 0
 ) {
-    init {
-        this.state = state
-        this.id = id
-        this.name = name
-        this.nameData = nameData
-        this.pos = pos
-    }
 }
