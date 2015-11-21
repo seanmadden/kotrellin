394dfcff0a3c77a84ce94183d0f60ef84c4e1393
Sean Madden <sdm350@gmail.com>
2015-11-20 23:44:14 -0800

    Refactor parameter src to card

--- src/serializers/CardSerializer.kt
+++ src/serializers/CardSerializer.kt
@@ -3,19 +3,22 @@ import com.github.salomonbrys.kotson.*
 import com.google.gson.*
 
 class CardSerializer() : JsonSerializer<Card> {
-    override fun serialize(src: Card?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement? {
-        if (src == null)
+    override fun serialize(card: Card?, typeOfSrc: Type?, context: JsonSerializationContext?): JsonElement? {
+        if (card == null)
             return JsonPrimitive("")
 
         val json = JsonObject()
-        json.add("id", JsonPrimitive(src.id))
+        json.add("id", JsonPrimitive(card.id))
+        json.add("badges", JsonPrimitive("TODO")) //TODO
+        json.add("checkItemStates", JsonArray()) //TODO
+        json.add("name", JsonPrimitive(card.name))
 
         //gather the checklist ids
         val array = JsonArray()
-        src.idChecklists.forEach { array.add(JsonPrimitive(it.id)) }
+        card.idChecklists.forEach { array.add(JsonPrimitive(it.id)) }
 
         json.add("idChecklists", array)
 
-        return json as JsonElement
+        return json
     }
 }
